22d58995ea85ce469efc03f611ca6ce669b779f7
Radek Wozniak <radeusz32@gmail.com>
2025-02-03 13:53:31 +0100

    poprawa kodu

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skracacz URL</title>
+    @vite('resources/js/app.js','resources/css/app.css')
+</head>
+<body>
+    <div id="app">
+        
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LinkController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app'); // Bez rozszerzenia .blade.php
 });
+
+Route::post('/links', [LinkController::class, 'store']);  // Tworzenie skróconego linku
+Route::get('/links', [LinkController::class, 'index']);  // Pobieranie listy linków
+Route::get('/{shortCode}', [LinkController::class, 'redirect']);
